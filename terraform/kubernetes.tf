246e42177c84a61d3e7d0c3237784fde7b169364
dave <dave@macmini.lan>
2021-01-01 15:50:09 -0500

    use t2.medium for th is

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -216,7 +216,7 @@ depends_on        = [ aws_vpc.kubernetes_vpc]
 
 resource "aws_instance" "kuber_master_ec2_instance" {
   ami           = "ami-0885b1f6bd170450c"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   iam_instance_profile = aws_iam_instance_profile.kubernetes_iam_aws_instance.name
   subnet_id = aws_subnet.kubernetes_public_subnet.id
   vpc_security_group_ids = [aws_security_group.allow_ssh.id,aws_security_group.kubernetes_default_sg.id]
@@ -231,7 +231,7 @@ resource "aws_instance" "kuber_master_ec2_instance" {
 
 resource "aws_instance" "kuber_node_ec2_instance" {
   ami           = "ami-0885b1f6bd170450c"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   subnet_id = aws_subnet.kubernetes_private_subnet.id
   iam_instance_profile = aws_iam_instance_profile.kubernetes_iam_aws_instance.name
   vpc_security_group_ids = [aws_security_group.allow_ssh.id,aws_security_group.kubernetes_default_sg.id]
